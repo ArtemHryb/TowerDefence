ea6d4a31a87143a9b155336501ca61337530c66e
ArtemHryb <artem.mushroom0201@gmail.com>
2024-05-07 17:00:08 +0300

    Add level1

---
+++ TowerDefence/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private Transform _finish;
+
+        private void Start()
+        {
+            MoveToFinish(_finish);
+        }
+
+        private void MoveToFinish(Transform finish)
+        {
+            _agent.SetDestination(finish.position);
+        }
+    }
+}
