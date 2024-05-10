90058ea8cf4c352192cb89fb3625a01dcd5f65d2
ArtemHryb <artem.hryb.gh@gmail.com>
2024-05-10 21:02:34 +0300

    Improve spawn system, Add EnemyCollision

--- TowerDefence/Assets/Scripts/Architecture/States/LoadGameState.cs
+++ TowerDefence/Assets/Scripts/Architecture/States/LoadGameState.cs
@@ -1,9 +1,5 @@
-﻿using Architecture.Services;
-using Architecture.Services.Enemy;
-using Architecture.Services.Factories.UI;
-using Architecture.Services.Interfaces;
+﻿using Architecture.Services.Factories.UI;
 using Architecture.States.Interfaces;
-using Data;
 using SceneManagement;
 using UnityEngine;
 
@@ -14,44 +10,24 @@ namespace Architecture.States
         private const string Level1 = "Level1";
         
         private readonly ISceneLoader _sceneLoader;
-        private readonly IEnemySpawner _enemySpawner;
-        private readonly ICurrentLevelSettingsProvider _currentLevelSettingsProvider;
         private readonly IUIFactory _uiFactory;
-        private readonly IAssetProvider _assetProvider;
 
-        public LoadGameState(ISceneLoader sceneLoader,IEnemySpawner enemySpawner,
-            ICurrentLevelSettingsProvider currentLevelSettingsProvider, IUIFactory uiFactory, IAssetProvider assetProvider)
+        public LoadGameState(ISceneLoader sceneLoader, IUIFactory uiFactory)
         {
             _sceneLoader = sceneLoader;
-            _enemySpawner = enemySpawner;
-            _currentLevelSettingsProvider = currentLevelSettingsProvider;
             _uiFactory = uiFactory;
-            _assetProvider = assetProvider;
         }
         public void Exit()
         {
         }
 
-        public void Enter()
-        {
+        public void Enter() => 
             _sceneLoader.Load(Level1, InitGame);
-        }
 
         private void InitGame()
         {
             Debug.Log("Level1");
             _uiFactory.CreateInGameMenu();
-            Object.Instantiate(_assetProvider.Initialize<Transform>(AssetPath.Cube), 
-                new Vector3(-123.9241f, 9.73f, -166f),Quaternion.identity);
-            
-            Object.Instantiate(_assetProvider.Initialize<Transform>(AssetPath.Cube), 
-                new Vector3(-123.9241f, 9.73f, -160f),Quaternion.identity);
-
-            //_enemySpawner.SpawnEnemies(Count,Waves,Delay);
-
-            // _enemySpawner.SpawnEnemies(_currentLevelSettingsProvider.GetCurrentLevelSettings().EnemyCount,
-            //     _currentLevelSettingsProvider.GetCurrentLevelSettings().Waves,
-            //     _currentLevelSettingsProvider.GetCurrentLevelSettings().DelayBeforeNextWave);
         }
     }
 }
---
+++ TowerDefence/Assets/Scripts/Enemy/EnemyCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyCollision : MonoBehaviour
+    {
+        private const string Finish = "Finish";
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(Finish))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
